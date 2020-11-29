300620ae459706ba503a37b40985df387c4989d7
Leo <lyx970124@gmail.com>
2020-11-29 16:34:59 -0600

    camera follow system and aspect ratio fix

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Func<Vector3> _getCameraFollowPosition;
+    public float left;
+    public float right;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 cameraFollowPosition = _getCameraFollowPosition();
+        cameraFollowPosition.z = transform.position.z;
+        transform.position = cameraFollowPosition;
+        
+        // set camera boundary
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, left, right), transform.position.y, transform.position.z);
+    }
+
+    public void Setup(Func<Vector3> getCameraFollowPosition)
+    {
+        _getCameraFollowPosition = getCameraFollowPosition;
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,20 +4,38 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public archer archerPrefab;
+	// setup camera follow system
+	private CameraFollow _cameraFollow;
+	private Vector3 _cameraFollowPosition;
+	
+	public archer archerPrefab;
+    
     // Start is called before the first frame update
     void Start()
     {
-       
+	    // setup camera follow system
+	    _cameraFollow = GameObject.FindWithTag("MainCamera").GetComponent<CameraFollow>();
+	    _cameraFollow.Setup(() => _cameraFollowPosition);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+	    // setup camera follow system
+        float movement = 10f;
+        float edgeSize = 50f;
+        if (Input.GetKey(KeyCode.LeftArrow))
+	        _cameraFollowPosition.x -= movement * Time.deltaTime;
+        if (Input.GetKey(KeyCode.RightArrow))
+	        _cameraFollowPosition.x += movement * Time.deltaTime;
+        if (Input.mousePosition.x > Screen.width - edgeSize)
+	        _cameraFollowPosition.x += movement * Time.deltaTime;
+        if (Input.mousePosition.x < edgeSize)
+	        _cameraFollowPosition.x -= movement * Time.deltaTime;
     }
-    public void generateArcher() {
-	Vector3 playerCastlePosition = GameObject.FindWithTag("PlayerCastle").transform.position;
-	Instantiate(archerPrefab, new Vector3(playerCastlePosition.x, playerCastlePosition.y-0.5f, 0), Quaternion.identity);
+    public void generateArcher()
+    {
+	    Vector3 playerCastlePosition = GameObject.FindWithTag("PlayerCastle").transform.position; 
+	    Instantiate(archerPrefab, new Vector3(playerCastlePosition.x, playerCastlePosition.y-0.5f, 0), Quaternion.identity);
     }
 }
