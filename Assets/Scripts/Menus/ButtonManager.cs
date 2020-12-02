253053b8896bc82ad62e721fe601d1df6c1a418c
akyyn1996 <57305982+akyyn1996@users.noreply.github.com>
2020-12-02 11:24:31 +0800

    Resume and Quit function in Pause Menu

---
+++ Assets/Scripts/Menus/ButtonManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void Quit()
+    {
+        SceneManager.LoadScene("Start");
+    }
+
+    
+
+}
